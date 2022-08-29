c1359b095b15ef38a938e140695d81882ccf0653
zengyuqing <676134152@qq.com>
2022-08-29 22:32:51 +0800

    更新路由

--- src/router/index.js
+++ src/router/index.js
@@ -15,38 +15,52 @@ const routes = [
       {
         // 这里不设置值，是把main作为默认页面
         path: "",
-        redirect: "/home",
-      },
-      {
-        // 这里不设置值，是把main作为默认页面
-        path: "home",
-        name: "Main",
-        component: () => import("../page/Main.vue"),
-      },
-      {
-        path: "user",
-        name: "User",
-        component: () => import("../page/User.vue"),
-      },
-      {
-        path: "articleList",
-        name: "articleList",
-        component: () => import("../page/article/ArticleList.vue"),
-      },
-      {
-        path: "test",
-        name: "test",
-        component: () => import("../page/Test.vue"),
+        redirect: "/dishList",
       },
       {
         path: "dishList",
         name: "DishList",
-        component: () => import("../page/dish-ms/dish/index.vue"),
+        component: () => import("../page/dish/dish/index.vue"),
       },
       {
         path: "dishForm",
         name: "DishForm",
-        component: () => import("../page/dish-ms/dish/form.vue"),
+        component: () => import("../page/dish/dish/form.vue"),
+      },
+      {
+        path: "comboList",
+        name: "ComboList",
+        component: () => import("../page/dish/combo/List.vue"),
+      },
+      {
+        path: "comboForm",
+        name: "ComboForm",
+        component: () => import("../page/dish/combo/Form.vue"),
+      },
+      {
+        path: "orderList",
+        name: "OrderList",
+        component: () => import("../page/order/List.vue"),
+      },
+      {
+        path: "storeList",
+        name: "StoreList",
+        component: () => import("../page/store/List.vue"),
+      },
+      {
+        path: "storeForm",
+        name: "StoreForm",
+        component: () => import("../page/store/Form.vue"),
+      },
+      {
+        path: "adminList",
+        name: "AdminList",
+        component: () => import("../page/authority/AdminList.vue"),
+      },
+      {
+        path: "adminForm",
+        name: "AdminForm",
+        component: () => import("../page/authority/AdminForm.vue"),
       },
     ],
   },
@@ -57,7 +71,6 @@ const router = createRouter({
   history: createWebHistory("/"),
   routes, // `routes: routes` 的缩写
 });
-
 
 //进行路由拦截：当没有登陆标识，直接打回登陆页面，如何避免退回到 登陆页呢？
 router.beforeEach((to, from, next) => {
@@ -77,11 +90,7 @@ export default router;
 
 export const sidebar = [
   {
-    title: "用户",
-    index: "user",
-  },
-  {
-    title: "菜品管理",
+    title: "菜品",
     index: "DishManager",
     subs: [
       {
@@ -95,16 +104,89 @@ export const sidebar = [
     ],
   },
   {
-    title: "文章",
+    title: "套餐",
+    index: "ComboManager",
     subs: [
       {
-        title: "文章列表",
-        index: "articleList",
+        title: "套餐列表",
+        index: "comboList",
+      },
+      {
+        title: "添加套餐",
+        index: "comboForm",
+      },
+    ],
+  },
+
+  {
+    title: "订单",
+    subs: [
+      {
+        title: "订单列表",
+        index: "orderList",
+      },
+      {
+        title: "待支付订单",
+        index: "orderList?orderType=1",
+      },
+      {
+        title: "确认中订单",
+        index: "orderList?orderType=2",
+      },
+      {
+        title: "备餐中订单",
+        index: "orderList?orderType=3",
+      },
+      {
+        title: "待用餐订单",
+        index: "orderList?orderType=4",
+      },
+      {
+        title: "待取餐订单",
+        index: "orderList?orderType=5",
+      },
+      {
+        title: "配送中订单",
+        index: "orderList?orderType=6",
+      },
+      {
+        title: "已完成订单",
+        index: "orderList?orderType=7",
+      },
+      {
+        title: "取消确认中订单",
+        index: "orderList?orderType=8",
+      },
+      {
+        title: "已取消订单",
+        index: "orderList?orderType=9",
       },
     ],
   },
   {
-    title: "测试",
-    index: "test",
+    title: "店铺",
+    subs: [
+      {
+        title: "店铺列表",
+        index: "storeList",
+      },
+      {
+        title: "添加店铺",
+        index: "storeForm",
+      },
+    ],
+  },
+  {
+    title: "权限",
+    subs: [
+      {
+        title: "用户管理",
+        index: "adminList",
+      },
+      {
+        title: "添加用户",
+        index: "adminForm",
+      },
+    ],
   },
 ];
